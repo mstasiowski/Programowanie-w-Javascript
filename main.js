9f30548b7e41d9deb7ef24c99192501d9aa5c622
mstasiowski <80395339+mstasiowski@users.noreply.github.com>
2022-10-05 12:50:39 +0200

    2

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ const liczba1 = document.querySelector('#liczba1');
 const btnPrzelicz = document.querySelector('#przelicz')
 const wynikiPojemnik = document.querySelector('#wyniki')
 
+const liczba2 = document.querySelector('#liczba2');
+const liczba3 = document.querySelector('#liczba3');
+const liczba4 = document.querySelector('#liczba4');
+
+
 btnPrzelicz.addEventListener('click', () => {
-    wynikiPojemnik.innerHTML= liczba1.value
+     
+
+  wynikiPojemnik.innerHTML = 
     console.dir(liczba1.value)
+    console.dir(liczba2.value)
+    console.dir(liczba3.value)
+    console.dir(liczba4.value)
+    
 })
