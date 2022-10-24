f16a2cd8803a5e934a5e5937383f69a042f299b6
mstasiowski <michal.stasiowski@microsoft.wsei.edu.pl>
2022-10-24 13:29:27 +0200

    3

--- lab3/main.js
+++ lab3/main.js
@@ -1,4 +1,19 @@
-document.addEventListener('keypress', onKeyPress)
+document.addEventListener('keypress', onKeyPress);
+
+let startrecordbtn = document.querySelector('#startrecord');
+let stoprecordbtn = document.querySelector('#stoprecord');
+let playrecordbtn = document.querySelector('#playtrack1')
+
+let recordingStartTime;
+let firstchannel= [];
+let secondchannel= [];
+let thirdchannel= [];
+let fourthchannel= [];
+
+startrecordbtn.addEventListener('click',startrecording);
+stoprecordbtn.addEventListener('click',stoprecording);
+playrecordbtn.addEventListener('click',playTrack1)
+
 
 // const Sounds = {
 //     'clap':'clap',
@@ -21,21 +36,23 @@ const KeyToSound = {
 function onKeyPress(ev){
  const  sound = KeyToSound[ev.key]
 
-//    const key = event.key
-//     logika mapowania key -> sound 
-//   switch
-// switch (ev.key) {
-//     case 'a':
-//     //clap
-//     sound = Sounds.clap
-//     break;
+  /* const key = event.key
+    logika mapowania key -> sound 
+  switch
+switch (ev.key) {
+    case 'a':
+    //clap
+    sound = Sounds.clap
+    break;
 
-//     case 's':
-//         sound = Sounds.clap2
-//     //clap2
-//     break;
-// }
-//   const sound = "clap"
+    case 's':
+        sound = Sounds.clap2
+    //clap2
+    break;
+}
+  const sound = "clap" */
+
+
   playSound(sound)
 }
 
@@ -46,10 +63,52 @@ function playSound(sound) {
     if(!sound){
         return 
     }
-    console.log(sound)
+    
+    if(recordStatus === true){
+      recordSound(sound)
+    }
+
   const audioTag = document.querySelector(`#${sound}`);
   audioTag.currentTime = 0
   audioTag.play()
 }
 
+
+let recordStatus = false;
+
+function startrecording(){
+  console.log("recording!");
+  console.log()
+  recordStatus = true;
+
+  recordingStartTime = Date.now();
+  firstchannel = [];
+  
+}
+
+function stoprecording() {
+  console.log("stop record!");
+  recordStatus = false;
+  
+}
+
+function playTrack1(){
+   if(firstchannel.length === 0) return 
+ console.log(firstchannel)
+
+firstchannel.forEach(track1 =>{
+  setTimeout(() => {
+    playSound(track1.key)
+  }, track1.startTime);
+})
+
+}
+
+
+function recordSound(sound){
+  firstchannel.push({
+    key:sound,
+    startTime: Date.now() - recordingStartTime
+  })
+}
 //Date.now()
