ff7f4a34891d5a7fff58b70313f3e21b44059877
mstasiowski <michal.stasiowski@microsoft.wsei.edu.pl>
2022-11-24 00:56:42 +0100

    6

--- lab6/main.js
+++ lab6/main.js
@@ -1,9 +1,19 @@
-window.addEventListener('deviceorientation', onDeviceMove)
-let ball = document.querySelector('.ball');
+//window.addEventListener('deviceorientation', onDeviceMove)
+let ball = document.querySelector('#ball');
+let  hole =document.querySelector('#hole');
+let map = document.querySelector('#map');
 
-function onDeviceMove(event) {
-    console.log(event)
-}
+
+const MaxX = map.clientWidth - ball.clientWidth;
+const MaxY = map.clientHeight - ball.clientHeight;
+// function onDeviceMove(event) {
+//     console.log(event)
+    
+// }
+
+
+
+
 
 function animate() {
        //console.log(Date.now())
@@ -11,3 +21,42 @@ function animate() {
 }
 
 requestAnimationFrame(animate)
+
+
+let SpeedX = 0;
+let SpeedY = 0;
+let positionX = 20;
+let positionY = 20;
+
+
+
+window.addEventListener('deviceorientation', (move)=>{
+    ball.style.top = `${(MaxY * positionY) / 180 - 10}px`;
+  ball.style.left = `${(MaxX * positionX) / 180 - 10}px`;
+
+   SpeedX = move.gamma/45;
+   SpeedY = move.beta/45;
+
+   
+
+
+
+    
+   if(positionX + SpeedX >0)
+   {
+    positionX= positionX +SpeedX;
+    ball.style.left = `${positionX}px`;
+   }
+
+   if(positionY + SpeedY >0)
+   {
+    positionY= positionY +SpeedY;
+    ball.style.top = `${positionY}px`;
+   }
+    
+   
+   
+    
+})
+
+
